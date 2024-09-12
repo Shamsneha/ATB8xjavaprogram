6584aba1df19f196e5618fd5b4e56df4be7e45ec
Neha Shams <shamsneha48@gmail.com>
2024-09-12 18:43:30 +0530

    ATB

---
+++ src/sept/ex_06092024/Lab003.java
@@ -0,0 +1,11 @@
+package sept.ex_06092024;
+
+import java.sql.SQLOutput;
+
+public class Lab003 {
+    public static void main(String[] args) {
+        System.out.println("Hello world");
+        System.out.println(2+2);
+        System.out.println(5-2);
+    }
+}
