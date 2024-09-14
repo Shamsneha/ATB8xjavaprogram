9594147b6329085ebdfa2d8024403bdcde6f534f
Neha Shams <shamsneha48@gmail.com>
2024-09-14 09:58:13 +0530

    ATB

---
+++ src/sept/ex_13092024/Lab010.java
@@ -0,0 +1,14 @@
+package sept.ex_13092024;
+
+public class Lab010 {
+    public static void main(String[] args) {
+//Difference b/w println and print is:
+  //ln for new line and only print is use for print anything in one line.
+        System.out.println("Hello Neha");
+        System.out.println("How are you");
+        System.out.print("Hello Neha");
+        System.out.print("How are you");
+
+    }
+
+}
---
+++ src/sept/ex_13092024/Lab012.java
@@ -0,0 +1,21 @@
+package sept.ex_13092024;
+
+public class Lab012 {
+    //Literals
+    //Boolean:true,false
+    Boolean is_married = true;
+    Boolean is_married_anu = false;
+    //Integral
+    //Decimal literal(10 base)
+    int n = 101;
+    //Octal literal(8 base)
+    int oct = 0101;
+    //Hexadecimal literal(16 base)
+    int hex = 0xFace;
+    //Binary literal
+    int x= 10101;
+
+    //Char
+    //Single char
+    //
+}
