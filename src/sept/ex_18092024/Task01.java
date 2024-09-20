002132916d34fc1a5ca588244a22c8ce3524c08f
Neha Shams <shamsneha48@gmail.com>
2024-09-20 06:52:25 +0530

    ATB

---
+++ src/sept/ex_18092024/Task01.java
@@ -0,0 +1,29 @@
+package sept.ex_18092024;
+
+public class Task01 {
+    public static void main(String[] args) {
+        /**Give some another example of Widening with Implicit and Explicit,
+         *  Narrowing with implicit and explicit.
+         **/
+        //Widening with implicit conversion
+        int a=50;
+        long b=(long)a;
+        float c=(float)b;
+        System.out.println(a);
+        System.out.println(b);
+        System.out.println(c);
+
+        //Narrowing with implicit and explicit.
+        //double->float->long->int->short->byte
+        double d = 100;
+        long l = (long)d;
+        int i = (int)l;
+        System.out.println(d);
+        // some  part lost
+        System.out.println(l);
+        // some part lost
+        System.out.println(i);
+
+
+    }
+}
